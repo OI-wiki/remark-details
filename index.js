b056b56a8d76ed8487aefff4f78524c6001afd5c
ahacad <ahacad@126.com>
2021-07-09 12:55:26 +0800

    feat(index.js): update

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
-'use strict'
+'use strict';
 
-const fromMarkdown = require('mdast-util-footnote/from-markdown')
-const toMarkdown = require('mdast-util-footnote/to-markdown')
-const warningIssued
+import { details as syntax } from './micromark-extension-details/index.js';
+// import fromMarkdown from 'mdast-util-footnote/from-markdown';
+// import toMarkdown from 'mdast-util-footnote/to-markdown';
 
-module.exports = details
+let warningIssued;
 
-function details(options) {
-    const data = this.data();
-    // warning for old remarks
-    if (!warningIssued &&
-        ((this.Parser && this.Parser.prototype &&
-          this.Parser.prototype.blockTokenizers) ||
-         (this.Compiler && this.Compiler.prototype &&
-          this.Compiler.prototype.visitors))) {
-        warningIssued = true
-        console.warn(
-            '[remark-footnotes] Warning: please upgrade to remark 13 to use this plugin')
-    }
+export default function details(options) {
+  const data = this.data();
+  // warning for old remarks
+  if (
+    !warningIssued &&
+    ((this.Parser &&
+      this.Parser.prototype &&
+      this.Parser.prototype.blockTokenizers) ||
+      (this.Compiler &&
+        this.Compiler.prototype &&
+        this.Compiler.prototype.visitors))
+  ) {
+    warningIssued = true;
+    console.warn(
+      '[remark-details] Warning: please upgrade to remark 13 to use this plugin',
+    );
+  }
 
-    // add('micromarkExtensions', syntax(options))
-    add('fromMarkdownExtensions', fromMarkdown)
-    add('toMarkdownExtensions', toMarkdown)
+  add('micromarkExtensions', syntax(options));
+  // add('fromMarkdownExtensions', fromMarkdown);
+  // add('toMarkdownExtensions', toMarkdown);
 
-    function add(field, value) {
-        /* istanbul ignore if - other extensions. */
-        if (data[field])
-            data[field].push(value)
-            else data[field] = [value]
-    }
+  function add(field, value) {
+    /* istanbul ignore if - other extensions. */
+    if (data[field]) data[field].push(value);
+    else data[field] = [value];
+  }
 }
