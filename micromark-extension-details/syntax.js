f5e9eaebf4932fd456ffaaf7aa5c76a42a82e3cb
ahacad <ahacad@126.com>
2021-07-11 18:14:38 +0800

    feat(micromak/syntax): finish parsing summary start symbols

--- micromark-extension-details/syntax.js
+++ micromark-extension-details/syntax.js
@@ -1,87 +1,88 @@
 import assert from 'assert';
-import { factorySpace } from 'micromark-factory-space';
-import { markdownSpace } from 'micromark-util-character';
-import { codes } from 'micromark-util-symbol/codes.js';
-import { constants } from 'micromark-util-symbol/constants.js';
-import { types } from 'micromark-util-symbol/types.js';
+import {factorySpace} from 'micromark-factory-space';
+import {markdownSpace} from 'micromark-util-character';
+import {codes} from 'micromark-util-symbol/codes.js';
+import {constants} from 'micromark-util-symbol/constants.js';
+import {types} from 'micromark-util-symbol/types.js';
+import {factorySummary} from './factory-summary.js';
 
-export const details = {
-  name: 'details',
-  tokenize: tokenizeDetails,
-  continuation: { tokenize: tokenizeDetailsContinuation },
-  exit,
+const detailsContainer = {
+    tokenize: tokenizeDetailsContainer,
+    concrete: true,
 };
-
-function tokenizeDetails(effects, ok, nok) {
-  const self = this;
-
-  return open1;
-
-  function open1(code) {
-    assert(code === codes.questionMark, 'expected `?`');
-    const state = selef.containerState;
-    assert(state, 'expected `containerState` to be defined in container');
-    if (!state.open) {
-      effects.enter('details', { _container: true });
-      state.open = true;
-    }
-    effects.enter('detailsPrefix');
-    effects.enter('detailsMarker');
-    effects.consume(code);
-    return open2;
-  }
-  function open2(code) {
-    return code === codes.questionMark ? open3(code) : nok(code);
-  }
-  function open3(code) {
-    if (code !== codes.questionMark) return nok(code);
-    effects.consume(code);
-    return isExpended;
-  }
-  function isExpended(code) {
-    if (code === codes.plusSign) {
-      effects.enter('detailsExpended');
-      effects.consume(code);
-      effects.exit('detailsExpended');
-      effects.exit('detailsMarker');
-    } else if (code === codes.space) {
-      effects.consume(code);
-      effects.exit('detailsMarker');
-      return summaryStart;
-    } else {
-      return nok(code);
-    }
-  }
-  function summaryStart(code) {
-    if (code === codes.lf) {
-      effects.exit('detailsPrefix');
-      return ok(code);
-    }
-    effects.enter('detailsSummary');
-    effects.consume(code);
-    return summaryInside;
-  }
-  function summaryInside(code) {
-    if (code === codes.lf) {
-      effects.exit('detailsSummary');
-      effects.exit('detailsPrefix');
-      return ok(code);
-    }
-    return summaryInside(code);
-  }
+export function details() {
+    return {
+        flow: {[codes.questionMark]: detailsContainer},
+    };
 }
 
-function tokenizeDetailsContinuation(effects, ok, nok) {
-  return factorySpace(
-    effects,
-    effects.attempt(details),
-    types.linePrefix,
-    this.parser.constructs.disable.null.includes('codeIndented')
-      ? undefined
-      : constants.tabSize,
-  );
-}
+function tokenizeDetailsContainer(effects, ok, nok) {
+    const self = this;
+    let sizeOpen = 0;
 
-function exit(effects) {
-  effects.exit('details');
-}
+    return open1;
+
+    function open1(code) {
+        assert(code === codes.questionMark, 'expected `?`');
+        effects.enter('detailsContainer');
+        effects.enter('detailsContainerFence');
+        effects.enter('detailsContainerSequence');
+        return open2(code);
+    }
+    function open2(code) {
+        if (code === codes.questionMark) {
+            effects.consume(code);
+            sizeOpen++;
+            return open2;
+        }
+        if (sizeOpen < constants.codeFencedSequenceSizeMin) {
+            return nok(code);
+        }
+        effects.exit('detailsContainerSequence');
+        return isExpanded;
+    }
+    function isExpended(code) {
+        if (code === codes.plusSign) {
+            effects.enter('detailsExpanded');
+            effects.consume(code);
+            effects.exit('detailsExpanded');
+        } else if (code === codes.space) {
+            effects.consume(code);
+        } else {
+            return nok(code);
+        }
+        return factorySummary.call(
+            self,
+            effects,
+            factorySpace(effects, afterSummay, types.whitespace),
+            nok,
+            'detailsContainerSummary',
+        );
+    }
+    /** @type {State} */
+    function afterSummary(code) {
+        effects.exit('detailsContainerFence');
+        if (code === codes.eof) {
+            return afterOpening(code);
+        }
+        // TODO: process line ending && start of content
+    }
+    /** @type {State} */
+    function afterSummary(code) {
+        function afterOpening(code) {
+            effects.exit('detailsContainer');
+            return ok(code);
+        }
+    }
+
+
+    function tokenizeDetailsContinuation(effects, ok, nok) {
+        return factorySpace(
+            effects,
+            effects.attempt(details),
+            types.linePrefix,
+            this.parser.constructs.disable.null.includes('codeIndented') ?
+                undefined :
+                constants.tabSize,
+        );
+    }
