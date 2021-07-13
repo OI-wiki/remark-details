7c7e993e2c45ddff04a3261c5938486ab82eaa9b
ahacad <ahacad@126.com>
2021-07-13 16:25:22 +0800

    chore(tests): add more test cases for test-micromark

--- tests/test-micromark.js
+++ tests/test-micromark.js
@@ -1,4 +1,5 @@
 import test from 'ava';
+import chalk from 'chalk';
 import { micromark } from 'micromark';
 
 import {
@@ -6,12 +7,36 @@ import {
   detailsHtml as html,
 } from '../micromark-extension-details/index.js';
 
-test('first', (t) => {
-  const value = `???+总结如下\ns`;
-  const a = micromark(value, {
-    extensions: [syntax()],
-    htmlExtensions: [html()],
+function T(num, value) {
+  test(`test case #${num}`, async (t) => {
+    const res = micromark(value, {
+      extensions: [syntax()],
+      htmlExtensions: [html()],
+    });
+    console.log(chalk.blue(`==Test case#${num}==`));
+    console.log(value);
+    console.log('=======>');
+    console.log(res);
+    console.log();
+    t.pass();
   });
-  console.log(a);
-  t.pass();
-});
+}
+
+T(1, `???+总结如下\nfind`);
+T(
+  2,
+  `???+总结如下
+    how to do the thing`,
+);
+T(
+  3,
+  `???+总结如下
+how to do the thing`,
+);
+T(
+  4,
+  `???+总结如下
+    how to do this
+    how to do that
+and that`,
+);
