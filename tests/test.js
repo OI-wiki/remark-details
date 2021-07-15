5de4c41b4aa6c99b0c892a435aacfa6fc0bd14e8
ahacad <ahacad@126.com>
2021-07-15 17:03:27 +0800

    chore(tests): add test cases

--- tests/test.js
+++ tests/test.js
@@ -84,3 +84,81 @@ T({
   expected: '<details open class="note"><summary>总结</summary></details>',
   message: 'no content details with note',
 })();
+T({
+  value: `???+ 总结
+    how to do this`,
+  expected:
+    '<details open><summary>总结</summary><p>how to do this</p></details>',
+  message: 'details without note',
+})();
+T({
+  value: `???+ note 总结
+    how to do this`,
+  expected:
+    '<details open class="note"><summary>总结</summary><p>how to do this</p></details>',
+  message: 'details with note',
+})();
+T({
+  value: `???+ note 总结
+    how to do this
+    how to do that`,
+  expected: `<details open class="note"><summary>总结</summary><p>how to do this
+how to do that</p></details>`,
+  message: 'details with note',
+})();
+T({
+  value: `???+ note 总结
+    to be or not to be
+    that is the question
+
+but now it is not`,
+  expected: `<details open class="note"><summary>总结</summary><p>to be or not to be
+that is the question</p></details>
+<p>but now it is not</p>`,
+  message: 'details with note',
+})();
+T({
+  value: `???+ note 总结
+    to be or not to be
+    that is the question
+
+but now it is not`,
+  expected: `<details open class="note"><summary>总结</summary><p>to be or not to be
+that is the question</p></details>
+<p>but now it is not</p>`,
+  message: 'details with note',
+})();
+T({
+  value: `???+ warning 总结
+    The sunlight claps the earth,
+    and the moonbeams kiss the sea:
+    what are all these kissings worth,
+    \`\`\`cpp
+    for (int i = 0; i <= 100; i++) {
+        cout << "if thou kiss not me?" << endl;
+    }
+    \`\`\`
+
+if not me, who`,
+  expected: `<details open class="warning"><summary>总结</summary><p>The sunlight claps the earth,
+and the moonbeams kiss the sea:
+what are all these kissings worth,</p><pre><code class="language-cpp">for (int i = 0; i &#x3C;= 100; i++) {
+    cout &#x3C;&#x3C; "if thou kiss not me?" &#x3C;&#x3C; endl;
+}
+</code></pre></details>
+<p>if not me, who</p>`,
+  message: 'details with note and code',
+})();
+T({
+  value: `???+ note 总结
+    > I have drunken deep of joy,
+    And I will taste no other wine tonight
+
+thats it`,
+  expected: `<details open class="note"><summary>总结</summary><blockquote>
+<p>I have drunken deep of joy,
+And I will taste no other wine tonight</p>
+</blockquote></details>
+<p>thats it</p>`,
+  message: 'details with note and blockquote',
+});
