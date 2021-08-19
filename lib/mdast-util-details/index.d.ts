ef559d1f93aafb316a3e696b8089ed08c712b611
true_mogician <925269713@qq.com>
2021-08-19 16:44:43 +0800

    chore: add lib files

---
+++ lib/mdast-util-details/index.d.ts
@@ -0,0 +1,3 @@
+import { Extension } from "mdast-util-from-markdown/lib";
+export declare const fromMarkdownDetails: Extension;
+export declare const detailsToMarkdown: {};
---
+++ lib/micromark-details/syntax.d.ts
@@ -0,0 +1,3 @@
+import { Extension } from 'micromark-util-types';
+declare const syntax: Extension;
+export default syntax;
